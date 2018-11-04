60edba1cfa627f3380bcd17a87c250dcc9340ec2
yk6 <grantex5@gmail.com>
2018-11-04 19:45:15 +0800

    Able to simulate: receive dissected floating point value, join them together and send back

--- FP_LUT.v
+++ FP_LUT.v
@@ -1,18 +1,18 @@
-module lab333333333333_v1_0//myip_v1_0 
-	(
-		// DO NOT EDIT BELOW THIS LINE ////////////////////
-		ACLK,
-		ARESETN,
-		S_AXIS_TREADY,
-		S_AXIS_TDATA,
-		S_AXIS_TLAST,
-		S_AXIS_TVALID,
-		M_AXIS_TVALID,
-		M_AXIS_TDATA,
-		M_AXIS_TLAST,
-		M_AXIS_TREADY
-		// DO NOT EDIT ABOVE THIS LINE ////////////////////
-	);
+module FP_LUT 
+  (
+    // DO NOT EDIT BELOW THIS LINE ////////////////////
+    ACLK,
+    ARESETN,
+    S_AXIS_TREADY,
+    S_AXIS_TDATA,
+    S_AXIS_TLAST,
+    S_AXIS_TVALID,
+    M_AXIS_TVALID,
+    M_AXIS_TDATA,
+    M_AXIS_TLAST,
+    M_AXIS_TREADY
+    // DO NOT EDIT ABOVE THIS LINE ////////////////////
+  );
 
 input                          ACLK;    // Synchronous clock
 input                          ARESETN; // System reset, active low
@@ -26,24 +26,41 @@ output                         M_AXIS_TVALID;  // Data out is valid
 output     [31 : 0]            M_AXIS_TDATA;   // Data Out
 output                         M_AXIS_TLAST;   // Optional data out qualifier
 input                          M_AXIS_TREADY;  // Connected slave device is ready to accept data out
-
+    
+   localparam FP = 3;
 
    // Total number of input data.
-   localparam NUMBER_OF_INPUT_WORDS  = 2;
+   localparam NUMBER_OF_INPUT_WORDS  = FP*1;
 
    // Total number of output data
-   localparam NUMBER_OF_OUTPUT_WORDS = 2;
+   localparam NUMBER_OF_OUTPUT_WORDS = FP/3;
 
    // Define the states of state machine
-   localparam Idle  = 4'b0100;
+   localparam Idle  = 4'b0001;
    localparam Read_Inputs = 4'b0010;
-   localparam Write_Outputs  = 4'b0001;
-   localparam Computing = 4'b1000;
-
+   localparam Compute = 4'b0100;
+   localparam Write_Outputs  = 4'b1000;
    reg [3:0] state;
 
    // Accumulator to hold sum of inputs read at any point in time
-   reg [31:0] sum;
+   // reg [31:0] first;
+   // reg [63:0] result;
+
+   reg sign[0:10];
+   reg [7:0] int[0:10];
+   reg [7:0] dec[0:10];
+   wire [16:0] value;
+   reg [16:0] temp[0:10];
+
+   assign value = temp[0];
+
+
+   reg [3:0] count;
+   
+   // wire [31:0] mul_in1;
+   // wire [31:0] mul_in2;
+   // assign mul_in1 = first;
+   // assign mul_in2 = S_AXIS_TDATA;
 
    // Counters to store the number inputs read & outputs written
    reg [NUMBER_OF_INPUT_WORDS - 1:0] nr_of_reads;
@@ -57,17 +74,7 @@ input                          M_AXIS_TREADY;  // Connected slave device is read
    assign M_AXIS_TVALID = (state == Write_Outputs);
    assign M_AXIS_TLAST = (state == Write_Outputs) & (nr_of_writes == 0);
 
-   assign M_AXIS_TDATA = sum;
-
-
-   // added variables
-   reg [63:0] result = 0;
-   reg [31:0] temp1 = 0;
-   reg [31:0] temp2 = 0;
-   reg done = 0;
-   reg read = 0;
-   reg send = 0;
-   reg [4:0]count = 31; 
+   assign M_AXIS_TDATA = M_AXIS_TVALID?value:0;
 
    always @(posedge ACLK) 
    begin
@@ -80,10 +87,6 @@ input                          M_AXIS_TREADY;  // Connected slave device is read
            state        <= Idle;
            nr_of_reads  <= 0;
            nr_of_writes <= 0;
-           sum          <= 0;
-           temp1        <= 0;
-           temp2        <= 0;
-           result       <= 0;
         end
       /************** state machines **************/
       else
@@ -93,64 +96,80 @@ input                          M_AXIS_TREADY;  // Connected slave device is read
             begin
               state       <= Read_Inputs;
               nr_of_reads <= NUMBER_OF_INPUT_WORDS - 1;
-              sum         <= 0;
+              count <= 0;
+              temp[0] <= 0;
             end
-
           Read_Inputs:
             if (S_AXIS_TVALID == 1) 
             begin
-              // --- Coprocessor function happens below --- //
-              //sum  <=  sum + S_AXIS_TDATA;
-              if (!read) begin
-                temp1 <= S_AXIS_TDATA;
-                read <= 1;
-              end else begin
-                temp2 <= S_AXIS_TDATA;
-                read <= 0;
+              if (nr_of_reads == 0) 
+              begin
+                state <= Compute;
+                count <= 0;
+                nr_of_writes <= NUMBER_OF_OUTPUT_WORDS - 1;
               end
-              // --- Coprocessor function happens above --- //
-              if (nr_of_reads == 0)
-                begin
-                  state        <= Computing; //Computing;
-                  nr_of_writes <= NUMBER_OF_OUTPUT_WORDS - 1;
-                end
-              else
-                nr_of_reads <= nr_of_reads - 1;
+              case(nr_of_reads%3)
+                0:
+                  begin
+                    dec[0] <= S_AXIS_TDATA;
+                    count <= count + 1;
+                    if (nr_of_reads != 0) 
+                    begin
+                      nr_of_reads <= nr_of_reads - 1;                      
+                    end
+                  end
+                1:
+                  begin
+                    int[0] <= S_AXIS_TDATA;
+                    nr_of_reads <= nr_of_reads - 1;
+                  end
+                2:
+                  begin
+                    sign[0] <= S_AXIS_TDATA;
+                    nr_of_reads <= nr_of_reads - 1;
+                  end
+                default:;
+              endcase
             end
-
           Write_Outputs:
             if (M_AXIS_TREADY == 1) 
             begin
-              if (!send) begin
-                sum <= result >> 32;
-                // sum <= result;
-                send <= 1;
-              end else begin
-                sum <= result & 32'hFFFFFFFF;
-                // sum <= temp2;
-                send <= 0;
+              if (nr_of_writes == 0)
+              begin
+                state <= Idle;
               end
-              if (nr_of_writes == 0) 
-                 state <= Idle;
-              else
-                 nr_of_writes <= nr_of_writes - 1;
             end
-          Computing:
-            if (!done) begin
-              result <= temp1 * temp2;
-              // result = temp2[0] ? (result + temp1): result;
-              // temp2 = temp2 >> 1;
-              // temp2[31] = result[0];
-              // result = result >> 1;
-              // count <= count - 1;
-              // if (count == 0) begin
-              //   done <= 1;
-              //   count <= 31;
-              // end
-            end else begin
-              state <= Write_Outputs;
-              // done <= 0;
-            end
+          Compute:
+          begin
+            if (nr_of_writes == 0)
+              begin
+                state <= Write_Outputs;
+              end
+              case (nr_of_writes%3)
+                0:
+                begin
+                  temp[0] <= sign[0] << 16 | int[0] << 8 | dec[0];                  
+                  count <= count + 1;                  
+                  if (nr_of_writes !=0) 
+                  begin
+                    nr_of_writes <= nr_of_writes - 1;
+                  end
+                end
+                1:
+                begin
+                  temp[0] <= sign[0] << 16 | int[0] << 8 | dec[0]; 
+                  count <= count + 1;
+                  nr_of_writes <= nr_of_writes - 1;
+                end
+                2:
+                begin
+                  temp[0] <= sign[0] << 16 | int[0] << 8 | dec[0]; 
+                  count <= count + 1;
+                  nr_of_writes <= nr_of_writes - 1;
+                end
+                default:;
+              endcase
+          end
         endcase
    end
 
---
+++ sim.v
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+
+module test(
+
+    );
+	reg ACLK, ARESETN;
+    reg [31:0]S_AXIS_TDATA;
+    reg S_AXIS_TVALID, M_AXIS_TREADY, S_AXIS_TLAST;
+    wire [31:0]M_AXIS_TDATA;
+    wire M_AXIS_TVALID, S_AXIS_TREADY;
+
+    FP_LUT dut(ACLK,
+        ARESETN,
+        S_AXIS_TREADY,
+        S_AXIS_TDATA,
+        S_AXIS_TLAST,
+        S_AXIS_TVALID,
+        M_AXIS_TVALID,
+        M_AXIS_TDATA,
+        M_AXIS_TLAST,
+        M_AXIS_TREADY);
+
+    reg i;
+    initial begin
+        ACLK = 0;
+        i = 0;
+        #3;
+        ARESETN = 0;
+        #20; 
+        ARESETN = 1; 
+        M_AXIS_TREADY = 1;
+        #8;
+        S_AXIS_TVALID = 1;
+        #10;
+        S_AXIS_TDATA = 1'b1;
+        #8;
+        S_AXIS_TDATA = 8'd200;
+        #12;
+        S_AXIS_TDATA = 8'd61;
+        #20
+        S_AXIS_TVALID = 0;
+        S_AXIS_TDATA = 0;
+    end
+
+    always begin
+    	#5; ACLK = ~ACLK;
+    end
+        
+        
+endmodule
