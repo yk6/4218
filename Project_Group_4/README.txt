37fbd8a535ef94ce331e0c6b486419078549cf9b
yk6 <grantex5@gmail.com>
2018-11-16 01:05:53 +0800

    Clean up and add updated proj file and zip

---
+++ Project_Group_4/README.txt
@@ -0,0 +1,14 @@
+- ee4218_project contains the vivado project for this project
+
+- ip_repo contains the customised IP that we implemented
+
+- the different verilog files are the different attempts we made to achieve better timing, the final implementation is "parallel 16bit Q8.8.v"
+
+- simulation.v is the testbench for the code, it is for simulation of Q10.10 format
+
+- sdk code.c contains the c code that  runs on the arm processor on the zedboard
+
+- ppt file self explainatory
+
+- pdf file is the reference for choosing the sigmoid implementation, PLAN is chosen by comparing its accuracy and difficulty to other implementation methods.
+
---
+++ Project_Group_4/ee4218_project/ee4218_project.sdk/project/Debug/src/helloworld.d
@@ -0,0 +1,66 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xaxidma.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xdebug.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xaxidma.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xdebug.h:
